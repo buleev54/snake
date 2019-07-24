1286dfe7958c82e52f1660737466351deae3b0fc
buleev54 <buleev.nsau@mail.ru>
2019-07-24 19:39:20 +0300

    убрал комментарии

--- Змейка/Program.cs
+++ Змейка/Program.cs
@@ -10,14 +10,11 @@ namespace Змейка
     {
         static void Main(string[] args)
         {          
-            Point p1 = new Point(1,3,'*');
-            /*p1.x = 1;
-            p1.y = 3;
-            p1.sym = '*';*/
+            Point p1 = new Point(1,3,'*');       
             p1.Draw();
 
-            Point p2 = new Point(4,5,'#');//создаем новую точку с некоторыми корами и символом      
-            p2.Draw();//выводим точку на экран
+            Point p2 = new Point(4,5,'#');
+            p2.Draw();
 
             Point p3 = new Point(10,15,'&');           
             p3.Draw();
