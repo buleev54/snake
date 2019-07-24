3c21259f31c883eff3b0df66f3f6b58cf209e789
buleev54 <buleev.nsau@mail.ru>
2019-07-24 16:31:20 +0300

    добавили точки с выводом их на экран

--- Змейка/Program.cs
+++ Змейка/Program.cs
@@ -10,7 +10,23 @@ namespace Змейка
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("привет");
+            int x1 = 1; //координата точки по оси Х
+            int y1 = 3; //координата точки по оси Y
+            char sym1 ='*'; // точка будет отображаться  в таком виде
+            Console.SetCursorPosition(x1, y1);// определяет положение курсора с заданными координатами
+            Console.Write(sym1);// выводит на экран точку в указанном виде
+
+            int x2 = 5; 
+            int y2 = 6; 
+            char sym2 = '#'; 
+            Console.SetCursorPosition(x2, y2);
+            Console.Write(sym2);
+
+            int x3 = 11; 
+            int y3 = 31; 
+            char sym3 = '&'; 
+            Console.SetCursorPosition(x3, y3);
+            Console.Write(sym3);
 
 
 
