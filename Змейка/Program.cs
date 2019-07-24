672da594d024848e50114c3b4d6b0be890991b3c
buleev54 <buleev.nsau@mail.ru>
2019-07-24 16:36:06 +0300

    добавил функцию Draw. Она отвечает за отображение точки на экране

--- Змейка/Program.cs
+++ Змейка/Program.cs
@@ -32,5 +32,11 @@ namespace Змейка
 
             Console.ReadKey();
         }
+
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
     }
 }
