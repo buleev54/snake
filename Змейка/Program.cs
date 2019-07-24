64b502954e190f14dbef6b8661730614c16e34ff
buleev54 <buleev.nsau@mail.ru>
2019-07-24 16:39:10 +0300

    ввел Draw для всех точек

--- Змейка/Program.cs
+++ Змейка/Program.cs
@@ -13,20 +13,20 @@ namespace Змейка
             int x1 = 1; //координата точки по оси Х
             int y1 = 3; //координата точки по оси Y
             char sym1 ='*'; // точка будет отображаться  в таком виде
-            Console.SetCursorPosition(x1, y1);// определяет положение курсора с заданными координатами
-            Console.Write(sym1);// выводит на экран точку в указанном виде
+            //Console.SetCursorPosition(x1, y1);// определяет положение курсора с заданными координатами
+            //Console.Write(sym1);// выводит на экран точку в указанном виде
+            Draw(x1, y1, sym1);
 
             int x2 = 5; 
             int y2 = 6; 
-            char sym2 = '#'; 
-            Console.SetCursorPosition(x2, y2);
-            Console.Write(sym2);
+            char sym2 = '#';
+            Draw(x2, y2, sym2);
+            
 
             int x3 = 11; 
             int y3 = 31; 
-            char sym3 = '&'; 
-            Console.SetCursorPosition(x3, y3);
-            Console.Write(sym3);
+            char sym3 = '&';
+            Draw(x3, y3, sym3);            
 
 
 
