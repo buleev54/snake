d2534ecd06aea0a43c62cc00d5c9d881c69186bc
buleev54 <buleev.nsau@mail.ru>
2019-07-24 16:51:46 +0300

    вывел точки с использованием класс Point и метода Draw

--- Змейка/Program.cs
+++ Змейка/Program.cs
@@ -9,24 +9,32 @@ namespace Змейка
     class Program
     {
         static void Main(string[] args)
-        {
-            int x1 = 1; //координата точки по оси Х
+        {          
+            /*int x1 = 1; //координата точки по оси Х
             int y1 = 3; //координата точки по оси Y
             char sym1 ='*'; // точка будет отображаться  в таком виде
             //Console.SetCursorPosition(x1, y1);// определяет положение курсора с заданными координатами
             //Console.Write(sym1);// выводит на экран точку в указанном виде
-            Draw(x1, y1, sym1);
+            Draw(x1, y1, sym1);*/
 
-            int x2 = 5; 
-            int y2 = 6; 
-            char sym2 = '#';
-            Draw(x2, y2, sym2);
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            Draw(p1.x, p1.y, p1.sym);
+
+            Point p2 = new Point();
+            p2.x = 5;
+            p2.y = 6;
+            p2.sym = '#';
+            Draw(p2.x, p2.y, p2.sym);
+
+            Point p3 = new Point();
+            p3.x = 11;
+            p3.y = 31;
+            p3.sym = '&';
+            Draw(p3.x, p3.y, p3.sym);
             
-
-            int x3 = 11; 
-            int y3 = 31; 
-            char sym3 = '&';
-            Draw(x3, y3, sym3);            
 
 
 
