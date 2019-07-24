cebc5b3020a93f2eed0e981e9dcc25ff43281737
buleev54 <buleev.nsau@mail.ru>
2019-07-24 16:54:34 +0300

    переместил метод Draw в класс Point

--- Змейка/Point.cs
+++ Змейка/Point.cs
@@ -11,5 +11,11 @@ namespace Змейка
         public int x;
         public int y;
         public char sym;
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
     }
 }
