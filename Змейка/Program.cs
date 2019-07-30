59e79dbc97c55db2f385a831572bb4b1b524583e
buleev54 <buleev.nsau@mail.ru>
2019-07-30 16:38:11 +0300

    ???

--- Змейка/Program.cs
+++ Змейка/Program.cs
@@ -26,7 +26,8 @@ namespace Змейка
 
             //отрисовка точек
             Point p = new Point(4, 5, '*');
-            p.Draw();
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Drew();  //вывод змейки на экран
 
 
 
